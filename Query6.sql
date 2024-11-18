6fbb1a52d720d9898c23e8c15aded742af9faaf2
Mohamed Mamdouh <momamdouh961@gmail.com>
2024-11-18 15:08:23 +0200

    Get number of vehicles made between 1950 and 2000 per make and add total vehicles column beside

---
+++ Query6.sql
@@ -0,0 +1,6 @@
+select	 Makes.Make, count(*) as NumberOfVehicles,(Select count(VehicleDetails.ID)  
+from		VehicleDetails)As TotalVehicles from VehicleDetails INNER JOIN 
+				Makes ON VehicleDetails.MakeID = Makes.MakeID
+where (VehicleDetails.Year between 1950 and 2000)
+group by Make
+order by NumberOfVehicles desc
